287f4f28f7752b29366615cfd3f2c6e6cbca845f
Jorge <jorge.cs1234@yomail.edu.pl>
2025-06-15 05:12:08 +0000

    cambia este logo que esta en la pantalla de inicio de sesion por un gorr

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -11,18 +11,18 @@ const Logo: React.FC<LogoProps> = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       width="24" // Default width, can be overridden by props
       height="24" // Default height, can be overridden by props
-      viewBox="0 0 24 24" // Standardized viewBox for the new icon
+      viewBox="0 0 24 24" // Standardized viewBox
       fill="none"
       stroke="currentColor" // Ensures it inherits text color
-      strokeWidth="2" // Standardized strokeWidth for the new icon
+      strokeWidth="2" // Standardized strokeWidth
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props} // Allows overriding default width, height, className, etc.
     >
-      {/* SVG paths for a graduation cap (Lucide GraduationCap icon) */}
-      <path d="M21.42 10.72L12 15.9L2.58 10.72L12 5.5Z"/>
-      <path d="M12 15.9V22L19 18.5"/>
-      <path d="M22 10.5V6C22 5.20435 21.6839 4.44129 21.1213 3.87868C20.5587 3.31607 19.7956 3 19 3H5C4.20435 3 3.44129 3.31607 2.87868 3.87868C2.31607 4.44129 2 5.20435 2 6V10.5"/>
+      {/* SVG paths for the minimalist graduation cap from the image */}
+      <path d="M2 9 L12 7 L22 9 L12 11 L2 9 Z" /> {/* Top mortarboard part */}
+      <path d="M7 11 L17 11 L17 15 L7 15 L7 11 Z" /> {/* Base hat part */}
+      <path d="M22 9 L22 12" /> {/* Tassel element */}
     </svg>
   );
 };
