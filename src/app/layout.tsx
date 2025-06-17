3800740786158dbc67bf74e7aa7f4ac73d7d2b9a
Superjf1234 <jorgefcs.1988@gmail.com>
2025-06-17 14:59:58 +0000

    feat: Actualizar favicon con nuevo diseño favicon_v2.png
    - Reemplazar favicon anterior con favicon_v2.png personalizado
    - Configurar referencias múltiples para máxima compatibilidad:
      * rel=icon para navegadores modernos
      * rel=shortcut icon para navegadores antiguos
      * rel=apple-touch-icon para dispositivos iOS
    - Usar formato PNG para mejor calidad y transparencia
    - El nuevo favicon se mostrará en pestañas del navegador

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning={true}>
       <head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon_v2.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon_v2.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon_v2.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
